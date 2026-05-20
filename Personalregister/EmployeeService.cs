4402f97a0bfaede965f320c510275e555b2579f5
Arjun <103857050+AM10101010@users.noreply.github.com>
2026-05-20 15:45:50 +0200

    Add EmployeeService with CRUD operations

---
+++ Personalregister/EmployeeService.cs
@@ -0,0 +1,36 @@
+using RestaurantEmployeeRegistry;
+
+public class EmployeeService
+{
+    private readonly List<Employee> _employees = new();
+
+    public void AddEmployee(Employee employee)
+    {
+        _employees.Add(employee);
+    }
+
+    public List<Employee> GetAllEmployees()
+    {
+        return _employees;
+    }
+    
+    public Employee? GetEmployeeById(int id)
+    {
+        return _employees.FirstOrDefault(e => e.Id == id);
+    }
+
+    public void RemoveEmployee(int id)
+    {
+        var employee = GetEmployeeById(id);
+        if (employee != null)
+        {
+            _employees.Remove(employee);
+        }
+    }
+
+    public List<Employee> FindByRole(Role role  )
+    {
+        return _employees.Where(e => e.Role == role).ToList();
+    } 
+
+}
